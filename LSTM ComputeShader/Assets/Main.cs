0490bc921d2273c02484c73fa2402b8818698233
randomuserhi <magicalworld.nano@gmail.com>
2021-05-14 05:06:17 +0100

    Fixed a bug where WeightBiasOffset was skipping a set

--- LSTM ComputeShader/Assets/Main.cs
+++ LSTM ComputeShader/Assets/Main.cs
@@ -9,18 +9,19 @@ public class Main : MonoBehaviour
 {
     LSTMManager.LSTMGroup LSTMGroup;
     ComputeShader LSTM;
-    int Population = 1000;
+    int Population = 1;
     float[] Inputs;
 
     public void Start()
     {
         Application.runInBackground = true;
-        Inputs = new float[2 * Population];
+        Inputs = new float[10 * Population];
         LSTM = LSTMManager.GenerateComputeShader();
-        LSTMGroup = LSTMManager.CreateLSTMGroup(new int[] { 2, 2 }, Population);
+        LSTMGroup = LSTMManager.CreateLSTMGroup(new int[] { 10, 10, 10, 10, 10 }, Population);
         LSTMManager.AssignLSTMGroupToShader(LSTMGroup, LSTM);
         LSTMGroup.Initialize();
 
+        Debug.Log(LSTMGroup.WeightsBiases.Length);
         for (int i = 0; i < LSTMGroup.WeightsBiases.Length; i++)
         {
             LSTMGroup.WeightsBiases[i] = 1;
@@ -39,6 +40,9 @@ public class Main : MonoBehaviour
     public void FixedUpdate()
     {
         float[] Result = LSTMManager.FeedForward(Inputs, LSTMGroup, LSTM);
+        Debug.Log("Update " + Count);
+        Debug.Log(String.Join(",", Result));
+        //LSTMGroup.DebugData();
         Count++;
     }
 
